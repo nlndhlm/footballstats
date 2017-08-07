998370bb9542055b859c31e86c381aeaba50cb11
nlndhlm <30705699+nlndhlm@users.noreply.github.com>
2017-08-07 17:34:10 +0200

    Update FOOTBALLSTATS.py

--- FOOTBALLSTATS.py
+++ FOOTBALLSTATS.py
@@ -12,3 +12,12 @@ teams = gt.get_teams(download_filename)
 
 ct.collect_stats(teams, download_filename, stats_filename)
 
+# URLs for other English leagues (16/17)
+# Championship: http://www.football-data.co.uk/mmz4281/1617/E1.csv
+# League One: http://www.football-data.co.uk/mmz4281/1617/E2.csv
+# League Two: http://www.football-data.co.uk/mmz4281/1617/E3.csv
+
+#NOTE: Will cause wrong numbers/error if there is no 'Referee'-column in the downloaded data.
+#This can be avoided by changing index of csv reader in collectstats.py
+
+
